fe9e00813e70cdee9629199bc2479a256bf92766
Thuventhiran Govender <thuventhiran@gmail.com>
2013-03-08 22:39:45 +0200

    Version 1.0
    This project is a commandline utility to facilitate generating scripts
    of SQL Server database objects (stored procedures, tables, views and
    functions).
    
    All objects (except tables) are scripted as "DROP-CREATE" scripts,
    meaning that if the object exists already, it will be dropped before the
    create script is run in order to prevent the need to manually drop the
    object or change the script to an ALTER statement.

---
+++ ScripTool/DomainLogic/SqlInstance.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SqlServer.Management.Smo;
+using Microsoft.SqlServer.Management.Sdk.Sfc;
+
+namespace ScripTool.DomainLogic
+{
+  public class SqlInstance
+  {
+    private SqlConnection _sqlConnection;
+
+    public SqlConnection SqlConnection
+    {
+      get { return _sqlConnection; }      
+    }
+    private Server _server;
+
+    public Server Server
+    {
+      get { return _server; }      
+    }
+
+    public SqlInstance(string connectionString)
+    {      
+      _sqlConnection = new SqlConnection(connectionString);
+      _server = new Server(new Microsoft.SqlServer.Management.Common.ServerConnection(_sqlConnection));      
+    }
+
+    
+  }
+}
---
+++ ScripTool/test.bat
@@ -0,0 +1,1 @@
+ScripTool.exe -c "Server=User-PC;Database=AdventureWorks;Trusted_Connection=True;" -o "E:\Users\Thuven\Desktop\test"
